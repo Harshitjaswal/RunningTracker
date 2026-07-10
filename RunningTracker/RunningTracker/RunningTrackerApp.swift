a2c5e22c50eebfd1119dffee2786b2f256f5d0da
Harshitjaswal <jaswalharshit2005@gmail.com>
2026-07-11 00:14:40 +0530

    chore: add folder structure

---
+++ RunningTracker/RunningTracker/RunningTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningTrackerApp.swift
+//  RunningTracker
+//
+//  Created by Harshit on 10/07/26.
+//
+
+import SwiftUI
+
+@main
+struct RunningTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
